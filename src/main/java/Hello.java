e49de57b18bcd19e89e8fe05ac7f7965659ab619
omprakash <omprakash.jaiswal@stixis.com>
2018-11-28 20:35:41 +0530

    # WARNING: head commit changed in the meantime
    d

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,16 +1,26 @@
 class Hello
 {
+  public  Hello(String m){
+		System.out.println("Hello.Hello()");
+	}
+
    public static void main(String[] args)
    {
+	
       String hello = "Hello", lo = "lo";
-      System.out.print((Other1.hello == hello) + " ");     //line 1 true
-      System.out.print((Other1.hello == "Hello") + " ");   //line 2 true
-      System.out.print((hello == ("Hel"+"lo")) + " ");       //line 3 true
-      System.out.print((hello == ("Hel"+lo)) + " ");         //line 4 false
+      String s = new String("Hello");
+    
+      System.out.println((Other1.hello == hello) + " ");     //line 1 true
+      System.out.println((Other1.hello == "Hello") + " ");   //line 2 true
+      System.out.println((hello == ("Hel"+"lo")) + " ");       //line 3 true
+      System.out.println((hello == ("Hel"+lo)) + " ");         //line 4 false
       System.out.println(hello == ("Hel"+lo).intern());      //line 5 true
       System.out.println(("Hel"+lo).hashCode());   //hashcode is 69609650 (machine depedent)
       System.out.println("Hello".hashCode());       //hashcode is same WHY ??.
    }
+   
 }
+
+
 
 class Other1 { static String hello = "Hello"; }
